859b5a194798b9f340d93d5702710645c4ca6c0d
RZuchniak <robert.zuchniak@hotmail.com>
2023-09-18 17:19:24 -0400

    feat: fixed navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,25 @@
 import {Link} from "react-router-dom";
-import './navbar.css'
+import './Navbar.css'
 function navbar() {
     return (
       <>
         <div id="links">
           <nav>
             <ul>
-              <li id="link1">
-                <Link className="text" to="/">Home</Link>
-              </li>
-              <li id="link2">
-              <Link className="text" to="/projects">Projects</Link>
-              </li>
+              
+                <li id="link1">
+                  <h2>
+                  <Link className="text" to="/">Home</Link>
+                  </h2>
+                </li>
+              
+                <li id="link2">
+
+                <h2>
+                <Link className="text" to="/projects">Projects</Link>
+
+                </h2>
+                </li>
             </ul>
           </nav>
         </div>
