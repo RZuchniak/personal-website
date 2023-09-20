d5de3e2096acb96a81cd312f8c28f2c39d80273c
Robert Zuchniak <robert.zuchniak@hotmail.com>
2023-09-19 23:08:37 -0400

    feat:started work on projects page

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,16 @@
+import './Project.css'
+export default function Project() {
+    return (
+        <>
+        <div className='card-box'>
+        <h2 className='Project-title'>
+        Title
+        </h2>
+        <p className='description'>
+        This is a project that I made
+        </p>
+        </div>
+        </>
+  )
+}
+  
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,10 +1,19 @@
+import './Projects.css'
 import Navbar from '../../components/Navbar/Navbar'
+import Project from '../../components/Project/Project'
 
 function Projects() {
     return(
         <>
         <div className='layout'>
          <Navbar/>
+         <div className='project-container'>
+         <Project/>
+         <Project/>
+         <Project/>
+         <Project/>
+         <Project/>
+         </div>
       </div>
         </>
     )
