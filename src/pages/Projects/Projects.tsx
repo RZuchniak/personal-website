5d47d2d3a62608536451753763790114abbbf87f
RZuchniak <robert.zuchniak@hotmail.com>
2023-09-26 15:47:57 -0400

    feat: fix project backgrounds

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -10,19 +10,19 @@ function Projects() {
             title: 'Personal Website',
             description: 'My first attempt at creating a personal website. Built with react and vite, with the intent of being improved and expanded down the road',
             link: 'https://github.com/RZuchniak/personal-website',
-            background: 'url("https://i0.wp.com/www.michigandaily.com/wp-content/uploads/2022/04/r_place-copy.jpg?fit=2400%2C1600&ssl=1")'
+            background: 'url("https://www.forbes.com/advisor/wp-content/uploads/2022/09/Image_-_How_to_design_a_website_.jpeg.jpg")'
         },
         {
             title: 'Mosaic',
             description: 'Based off of r/Place, Mosaic allows users to place individual tiles on a board to slowly create art. Built with react for the front-end, with express and postgress for the back-end',
             link: 'https://github.com/RZuchniak/mosaic_front-end',
-            background: 'https://c.ndtvimg.com/2022-04/9o7fvquo_reddit-r-place-artwork-650_625x300_02_April_22.jpg?im=Resize=(1230,900)'
+            background: 'url("https://i0.wp.com/www.michigandaily.com/wp-content/uploads/2022/04/r_place-copy.jpg?fit=2400%2C1600&ssl=1")'
         },
         {
             title: 'A* Maze Visualizer',
             description: 'Using the A* algorithm this program visualizes the shortest path possible to solve a maze. It was built in Python and uses Pygame to display graphics.',
             link: 'https://github.com/RZuchniak/maze-visualizer',
-            background: 'https://c.ndtvimg.com/2022-04/9o7fvquo_reddit-r-place-artwork-650_625x300_02_April_22.jpg?im=Resize=(1230,900)'
+            background: 'url(./maze.png)'
         }
     ]
 
